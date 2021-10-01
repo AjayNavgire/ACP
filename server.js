11a459105441001d58f4aff16d8c632d9d0ca915
Ajay Navgire <ajaynawgire@live.com>
2021-10-01 07:22:09 +0530

    feat: file added

---
+++ server.js
@@ -0,0 +1,16 @@
+//
+const express = require ("express")
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+      return res.send('Welcome to ACP server')
+      
+}) 
+
+app.listen(port, (error, result) => {
+    if (error) { 
+       console.log('server error', error)
+    }
+    console.log(`server is listening on ${port}`)
+})
